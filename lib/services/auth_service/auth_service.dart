873e56abb2b7ed2da22818028b20bf8a8f5ef067
Tomas Baran <tomas.baran@gmail.com>
2023-08-02 21:37:53 -0600

    refactor: remove unnecessary context parameter
    where: from signInWithGoogle() method in authService

--- lib/services/auth_service/auth_service.dart
+++ lib/services/auth_service/auth_service.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
 abstract class AuthService {
   FirebaseAuth get auth;
 
   String? get uid;
 
-  Future<UserCredential> signInWithGoogle(BuildContext context);
+  Future<UserCredential> signInWithGoogle();
 
   logout();
 }
--- lib/services/auth_service/firebase_auth_service.dart
+++ lib/services/auth_service/firebase_auth_service.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/calendar/v3.dart';
 import 'package:googleapis/tasks/v1.dart';
@@ -19,7 +18,7 @@ class FirebaseAuthService extends AuthService {
 
   // function to implement the google signin
   @override
-  Future<UserCredential> signInWithGoogle(BuildContext context) async {
+  Future<UserCredential> signInWithGoogle() async {
     GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 
     // Obtain the auth details from the request
