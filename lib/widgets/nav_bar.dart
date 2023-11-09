d1b721ac5a1d815028d0f0c65cd8738199ebf879
Tomas Baran <tomas.baran@gmail.com>
2023-11-08 19:23:02 -0600

    style: cleaning the navBar UI-wise

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -22,17 +22,17 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        GestureDetector(
-          // Flutter BUG: https://github.com/flutter/flutter/issues/128530
-          // onTap: () => widgetManager.pageController.animateToPage(todayIndex, duration: Duration(milliseconds: 300), curve: Curves.easeIn),
-          onTap: () => listState.selectDateListByDate(DateTime.now()),
-          behavior: HitTestBehavior.translucent,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 28, top: 16, bottom: 16, right: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            // Flutter BUG: https://github.com/flutter/flutter/issues/128530
+            // onTap: () => widgetManager.pageController.animateToPage(todayIndex, duration: Duration(milliseconds: 300), curve: Curves.easeIn),
+            onTap: () => listState.selectDateListByDate(DateTime.now()),
+            behavior: HitTestBehavior.translucent,
             child: Transform.translate(
               // angle: 0.18 * 3.1415926535897932, // Rotate 45 degrees (0.25 * 2 * pi)
               // angle: 0 * 3.1415926535897932, // Rotate 45 degrees (0.25 * 2 * pi)
@@ -40,11 +40,11 @@ class NavBar extends StatelessWidget {
               child: ValueListenableBuilder(
                   valueListenable: dateState.isSelectedDateToday,
                   builder: (_, isSelectedToday, __) {
-                    return CustomIcon(
-                      imagePath: isSelectedToday ? 'assets/icons/hummingbird-filled.png' : 'assets/icons/hummingbird-outlined.png',
-                      size: 30,
-                      color: Colors.blue,
-                    );
+                    // return CustomIcon(
+                    //   imagePath: /* isSelectedToday ? 'assets/icons/hummingbird-filled.png' :  */ 'assets/icons/hummingbird-outlined.png',
+                    //   size: 30,
+                    //   color: isSelectedToday ? kTodayColor : kIconColor,
+                    // );
                     return Icon(
                       // Icons.public,
 
@@ -54,119 +54,107 @@ class NavBar extends StatelessWidget {
                       // isSelectedToday ? CupertinoIcons.clock_fill : CupertinoIcons.clock,
                       // Icons.history,
 
+                      Icons.description_outlined,
                       // isSelectedToday ? Icons.description : Icons.description_outlined,
                       // isSelectedToday ? CupertinoIcons.doc_text_fill : CupertinoIcons.doc_text,
                       // CupertinoIcons.doc_plaintext,
                       // Icons.insert_drive_file_outlined,
 
-                      isSelectedToday ? CupertinoIcons.sunrise_fill : CupertinoIcons.sunrise,
+                      // isSelectedToday ? CupertinoIcons.sunrise_fill : CupertinoIcons.sunrise,
                       // isSelectedToday ? Icons.wb_sunny : Icons.wb_sunny_outlined,
                       // isSelectedToday ? CupertinoIcons.sun_max_fill : CupertinoIcons.sun_max,
                       // isSelectedToday ? CupertinoIcons.sun_haze_fill : CupertinoIcons.sun_haze,
 
                       // size: 28,
                       size: 31,
-                      color: kIconColor,
+                      color: isSelectedToday ? kTodayColor : kIconColor,
                     );
                   }),
             ),
           ),
-        ),
-        GestureDetector(
-          onTap: () => appState.updateNavBarSelection(NavBarSelection.calendar),
-          behavior: HitTestBehavior.translucent,
-          child: const Padding(
-            padding: EdgeInsets.all(16),
+          GestureDetector(
+            onTap: () => appState.updateNavBarSelection(NavBarSelection.calendar),
+            behavior: HitTestBehavior.translucent,
             child: Padding(
-              padding: EdgeInsets.only(bottom: 2),
+              padding: EdgeInsets.only(bottom: 2.0),
               child: Icon(
-                CupertinoIcons.calendar,
-                // Icons.calendar_today_outlined,
+                // CupertinoIcons.calendar,
+                Icons.calendar_month_outlined,
                 color: kIconColor,
-                size: 28,
+                size: 32,
               ),
             ),
           ),
-        ),
-        // Icon(
-        //   CupertinoIcons.calendar,
-        //   color: kThemeColor7,
-        //   size: 30,
-        // ),
-        GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: () => showCupertinoModalBottomSheet(
-            context: context,
-            builder: (context) => Scaffold(
-              body: TaskDetailSheet.newTask(),
+          // Icon(
+          //   CupertinoIcons.calendar,
+          //   color: kThemeColor7,
+          //   size: 30,
+          // ),
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () => showCupertinoModalBottomSheet(
+              context: context,
+              builder: (context) => Scaffold(
+                body: TaskDetailSheet.newTask(),
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
             child: Container(
-              height: 30,
-              width: 30,
+              height: 32,
+              width: 32,
               decoration: BoxDecoration(
-                // color: kIconColor,
-                border: Border.all(
-                  width: 2.3,
-                  color: kIconColor,
-                ),
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                // color: kBackgroundColor,
+                color: kIconColor,
+                // border: Border.all(
+                //   width: 2.3,
+                //   // color: kBackgroundColor,
+                //   color: kIconColor,
+                // ),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
               ),
               child: const Center(
                 child: Icon(
                   Icons.add_rounded,
-                  weight: 10,
-                  size: 23,
-                  color: kIconColor,
-                  // color: kThemeColor12,
+                  weight: 30,
+                  size: 32,
+                  // color: kIconColor,
+                  color: kThemeColor11,
                 ),
               ),
             ),
           ),
-        ),
 
-        // Hamburger icon
-        GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: () => appState.updateNavBarSelection(NavBarSelection.list),
-          // child: Text(
-          //   '&',
-          //   style: bottomToolbarIconTextStyle,
-          // ),
-          child: const Padding(
-            padding: EdgeInsets.all(16),
+          // Hamburger icon
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () => appState.updateNavBarSelection(NavBarSelection.list),
+            // child: Text(
+            //   '&',
+            //   style: bottomToolbarIconTextStyle,
+            // ),
             child: Icon(
               // Icons.flash_auto,
-              CupertinoIcons.line_horizontal_3_decrease,
+              // CupertinoIcons.line_horizontal_3_decrease,
               // Icons.notes_rounded,
+              Icons.filter_list_rounded,
+
               color: kIconColor,
               size: 30,
             ),
           ),
-        ),
-        GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: () => appState.updateNavBarSelection(NavBarSelection.account),
-          child:
-              // Icons.messenger_outline_sharp,
-              // CupertinoIcons.bubble_left,
-              const Padding(
-            padding: EdgeInsets.only(right: 32, top: 16, bottom: 16, left: 16),
-            child: FaIcon(
-              // Icons.account_circle_outlined,
-              // CupertinoIcons.person,
-              FontAwesomeIcons.user,
-              // FontAwesomeIcons.paperPlane,
-              // FontAwesomeIcons.commentDots,
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () => appState.updateNavBarSelection(NavBarSelection.account),
+            child:
+                // Icons.messenger_outline_sharp,
+                // CupertinoIcons.bubble_left,
+                Icon(
+              Icons.person_outline,
+              size: 30,
               color: kIconColor,
-              // size: 22,
-              size: 24,
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
