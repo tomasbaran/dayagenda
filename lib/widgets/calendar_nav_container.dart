71af075c8d017e35f1212c76156014f2d96c0871
Tomas Baran <tomas.baran@gmail.com>
2023-11-21 12:36:33 -0600

    feat: show dot in calendar part 1
    example

--- lib/widgets/calendar_nav_container.dart
+++ lib/widgets/calendar_nav_container.dart
@@ -27,16 +27,53 @@ class CalendarNavContainer extends StatelessWidget {
           const SizedBox(height: 24),
           CalendarDatePicker2(
               config: CalendarDatePicker2Config(
-                // cancelButton: ,
-                firstDayOfWeek: 0,
-                dayTextStyle: const TextStyle(color: kThemeColor2),
-                disableModePicker: true,
-                controlsTextStyle: const TextStyle(color: kThemeColor9, fontWeight: FontWeight.w800),
-                weekdayLabelTextStyle: const TextStyle(color: kThemeColor9, fontWeight: FontWeight.w800),
-                selectedDayTextStyle: const TextStyle(color: kThemeColor11, fontWeight: FontWeight.w700),
-                nextMonthIcon: const Icon(Icons.arrow_forward_ios_rounded, size: 20, color: kThemeColor9),
-                lastMonthIcon: const Icon(Icons.arrow_back_ios_rounded, size: 20, color: kThemeColor9),
-              ),
+                  // cancelButton: ,
+                  firstDayOfWeek: 0,
+                  dayTextStyle: const TextStyle(color: kThemeColor2),
+                  disableModePicker: true,
+                  controlsTextStyle: const TextStyle(color: kThemeColor9, fontWeight: FontWeight.w800),
+                  weekdayLabelTextStyle: const TextStyle(color: kThemeColor9, fontWeight: FontWeight.w800),
+                  selectedDayTextStyle: const TextStyle(color: kThemeColor11, fontWeight: FontWeight.w700),
+                  nextMonthIcon: const Icon(Icons.arrow_forward_ios_rounded, size: 20, color: kThemeColor9),
+                  lastMonthIcon: const Icon(Icons.arrow_back_ios_rounded, size: 20, color: kThemeColor9),
+                  dayBuilder: ({
+                    required date,
+                    textStyle,
+                    decoration,
+                    isSelected,
+                    isDisabled,
+                    isToday,
+                  }) {
+                    Widget? dayWidget;
+                    if (date.day % 3 == 0 && date.day % 9 != 0) {
+                      dayWidget = Container(
+                        decoration: decoration,
+                        child: Center(
+                          child: Stack(
+                            alignment: AlignmentDirectional.center,
+                            children: [
+                              Text(
+                                MaterialLocalizations.of(context).formatDecimal(date.day),
+                                style: textStyle,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 27.5),
+                                child: Container(
+                                  height: 4,
+                                  width: 4,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: isSelected == true ? Colors.white : Colors.grey[500],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                    return dayWidget;
+                  }),
               value: [dateState.selectedDate.value],
               onValueChanged: (dates) {
                 listState.selectDateListByDate(dates.first!);
