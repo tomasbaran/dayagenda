a0fe2a9c683113ef43dea97bbd7df11b155350fe
Tomas Baran <tomas.baran@rethoughtinsurance.com>
2024-01-02 16:15:46 -0600

    style: make the bottom padding of Pending and Completed the same

--- lib/widgets/completed_tasks_column.dart
+++ lib/widgets/completed_tasks_column.dart
@@ -18,29 +18,22 @@ class CompletedTasksColumn extends StatelessWidget {
     List<Widget> output = [];
     output.add(
       Padding(
-        padding: EdgeInsets.only(top: 8, left: 16, right: 16
-            // if the selected list is before today, don't show the completedTasksColumn top padding
-            // listState.dateState.selectedDate.value.isBefore(DateTimeUtils.resetTimeToZero(DateTime.now()))
-            //     ? 8
-            //     : appState.emptySpaceHeight(listState.selectedList.value.tasks.length, context) < minEmptySpaceHeight
-            //         ? minEmptySpaceHeight
-            //         : appState.emptySpaceHeight(listState.selectedList.value.tasks.length, context)
-            ),
+        padding: EdgeInsets.only(
+          top: 8, left: 16, right: 16, bottom: 8,
+          // if the selected list is before today, don't show the completedTasksColumn top padding
+          // listState.dateState.selectedDate.value.isBefore(DateTimeUtils.resetTimeToZero(DateTime.now()))
+          //     ? 8
+          //     : appState.emptySpaceHeight(listState.selectedList.value.tasks.length, context) < minEmptySpaceHeight
+          //         ? minEmptySpaceHeight
+          //         : appState.emptySpaceHeight(listState.selectedList.value.tasks.length, context)
+        ),
         child: Text(
           'COMPLETED: ${listState.selectedList.value.completedTasks.length}',
           style: taskCardSubtitleTextStyle,
         ),
       ),
     );
-    if (listState.selectedList.value.completedTasks.isNotEmpty) {
-      output.add(SizedBox(
-          height: // if the selected list is before today, don't show the completedTasksColumn top padding
-              listState.dateState.selectedDate.value.isBefore(DateTimeUtils.resetTimeToZero(DateTime.now()))
-                  ? 8
-                  : UniversalPlatform.isAndroid || UniversalPlatform.isIOS
-                      ? mobileCompletedTitleBottomPadding
-                      : desktopCompletedTitleBottomPadding));
-    }
+
     for (var completedTask in listState.selectedList.value.completedTasks) {
       output.add(TaskCard(
         task: completedTask,
