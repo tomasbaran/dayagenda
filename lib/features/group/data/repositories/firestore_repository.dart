2c1a719458206ff100ab9ac5a976180cfe103fa8
Tomas Baran <tomas.baran@gmail.com>
2024-02-24 21:54:46 -0600

    feat: Add createGroup() service

--- lib/features/group/data/repositories/firestore_repository.dart
+++ lib/features/group/data/repositories/firestore_repository.dart
@@ -1,17 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dayagenda/features/group/domain/entities/group.dart';
 import 'package:dayagenda/features/group/domain/entities/owner.dart';
 
 class FirestoreRepository {
   final FirebaseFirestore db;
   FirestoreRepository({required this.db});
 
-  Future<bool> createOwnerInDb(Owner owner) async {
+  Future<bool> createOwner(Owner owner) async {
     print('Adding owner to db...');
     final parsedOwner = {
       'uid': owner.uid,
       'email': owner.email,
     };
-    db.collection('owners').doc(owner.uid).set(parsedOwner);
+    await db.collection('owners').doc(owner.uid).set(parsedOwner);
     return true;
+  }
+
+  Future<String> createGroup(Group group) async {
+    print('Adding group to db...');
+    final parsedGroup = {
+      'name': group.name,
+      'owner_uid': group.ownerUid,
+    };
+    final ref = await db.collection('groups').add(parsedGroup);
+    return ref.id;
   }
 }
