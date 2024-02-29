ac6eedbc9350553a74f96bcf06f2e2dae6a16e7b
Tomas Baran <tomas.baran@gmail.com>
2024-02-28 21:51:52 -0600

    feat: Add equatable to Employee equity

--- lib/features/add_employee/domain/entities/employee.dart
+++ lib/features/add_employee/domain/entities/employee.dart
@@ -1,6 +1,7 @@
 import 'package:dayagenda/features/add_employee/domain/entities/employee_status.dart';
+import 'package:equatable/equatable.dart';
 
-class Employee {
+class Employee extends Equatable {
   String? uid;
   String firstName;
   String? secondName;
@@ -18,4 +19,10 @@ class Employee {
     this.email,
     required this.phone,
   });
+
+  @override
+  List<Object?> get props => [uid, firstName, secondName, thirdName, email, phone, status];
+
+  @override
+  bool get stringify => true;
 }
