ddf8cd68c63679add96b15f4fe823852659478d5
Tomas Baran <tomas.baran@gmail.com>
2024-02-28 19:56:48 -0600

    feat: Add AddEmployeeToEmployeesCollectionUsecase

---
+++ lib/features/group/domain/usecases/add_employee_to_employees_collection_usecase.dart
@@ -0,0 +1,11 @@
+import 'package:dayagenda/features/add_employee/domain/entities/employee.dart';
+import 'package:dayagenda/features/group/data/repositories/firestore_repository.dart';
+
+class AddEmployeeToEmployeesCollectionUsecase {
+  final FirestoreRepository _repository;
+  AddEmployeeToEmployeesCollectionUsecase(this._repository);
+
+  Future<void> call(Employee employee) async {
+    return _repository.addEmployeeToEmployeesCollection(employee);
+  }
+}
