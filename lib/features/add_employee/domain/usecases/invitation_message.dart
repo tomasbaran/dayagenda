2d395742c2bb6187960b9491ab2ade9793af94c1
Tomas Baran <tomas.baran@gmail.com>
2024-03-30 20:00:05 -0600

    fix: Update invitation message url to employee tmpId

--- lib/features/add_employee/domain/usecases/invitation_message.dart
+++ lib/features/add_employee/domain/usecases/invitation_message.dart
@@ -3,5 +3,5 @@ import 'package:dayagenda/globals/constants.dart';
 
 class InvitationMessage {
   static String call(Employee employee) =>
-      'Buenos días! Vamos a probar una herramienta para aumentar la productividad. Favor de registrarse acá: $kDayagendaUrl/#/register_employee/${employee.uid}. Gracias de antemano. Saludos!';
+      'Buenos días! Vamos a probar una herramienta para aumentar la productividad. Favor de registrarse acá: $kDayagendaUrl/#/register_employee/${employee.tmpId}. Gracias de antemano. Saludos!';
 }
